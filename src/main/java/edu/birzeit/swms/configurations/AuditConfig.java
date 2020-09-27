76534da8402901633a73d14e878d5e099bac2f61
Abufarhah <laythabufarhah@gmail.com>
2020-09-27 21:07:04 +0300

    auditing configurations

---
+++ src/main/java/edu/birzeit/swms/configurations/AuditConfig.java
@@ -0,0 +1,17 @@
+package edu.birzeit.swms.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+
+}
--- src/main/java/edu/birzeit/swms/models/Bin.java
+++ src/main/java/edu/birzeit/swms/models/Bin.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Bin {
 
     @Id
