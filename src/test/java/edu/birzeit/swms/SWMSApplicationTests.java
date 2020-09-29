ffb50faf847fe34ced2b0218dbbb9063eb6eecfa
Abufarhah <laythabufarhah@gmail.com>
2020-09-30 00:04:46 +0300

    fix test issue

--- src/test/java/edu/birzeit/swms/SWMSApplicationTests.java
+++ src/test/java/edu/birzeit/swms/SWMSApplicationTests.java
@@ -1,20 +1,8 @@
 package edu.birzeit.swms;
 
-import edu.birzeit.swms.enums.Status;
-import edu.birzeit.swms.models.Bin;
-import edu.birzeit.swms.repositories.BinRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SWMSApplicationTests {
-
-    @Autowired
-    BinRepository binRepository;
-
-    @Test
-    void contextLoads() {
-    }
 
 }
