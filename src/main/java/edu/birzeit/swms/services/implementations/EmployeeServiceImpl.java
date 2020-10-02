43ee2da2d8244abc2632f1f9e63d44825a504f7c
Abufarhah <laythabufarhah@gmail.com>
2020-10-02 14:51:36 +0300

    add needed classes

---
+++ src/main/java/edu/birzeit/swms/services/implementations/EmployeeServiceImpl.java
@@ -0,0 +1,9 @@
+package edu.birzeit.swms.services.implementations;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+@Log
+@Service
+public class EmployeeServiceImpl {
+}
