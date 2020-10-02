96f95eaff6465e1e7693b3e913d466ffd2860816
Abufarhah <laythabufarhah@gmail.com>
2020-10-02 16:39:31 +0300

    add employee stuff

--- src/main/java/edu/birzeit/swms/models/Employee.java
+++ src/main/java/edu/birzeit/swms/models/Employee.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
