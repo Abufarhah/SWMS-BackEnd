25f01276f429bf350e75110dda972902d66e187e
Abufarhah <laythabufarhah@gmail.com>
2020-10-06 23:18:35 +0300

    add vehicle controller

--- src/main/java/edu/birzeit/swms/controllers/VehicleController.java
+++ src/main/java/edu/birzeit/swms/controllers/VehicleController.java
@@ -1,11 +1,16 @@
 package edu.birzeit.swms.controllers;
 
+import edu.birzeit.swms.dtos.VehicleDto;
+import edu.birzeit.swms.services.VehicleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.java.Log;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Log
 @RestController
@@ -17,4 +22,34 @@ import org.springframework.web.bind.annotation.RestController;
         @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
 })
 public class VehicleController {
+
+    @Autowired
+    VehicleService vehicleService;
+
+    @GetMapping
+    public List<VehicleDto> getVehicles() {
+        return vehicleService.getVehicles();
+    }
+
+    @GetMapping("/{id}")
+    public VehicleDto getVehicle(@PathVariable int id) {
+        return vehicleService.getVehicle(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public VehicleDto addVehicle(@RequestBody VehicleDto vehicleDto) {
+        return vehicleService.addVehicle(vehicleDto);
+    }
+
+    @PutMapping("/{id}")
+    public VehicleDto updateVehicle(@RequestBody VehicleDto vehicleDto, @PathVariable int id) {
+        return vehicleService.updateVehicle(vehicleDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteVehicle(@PathVariable int id) {
+        vehicleService.deleteVehicle(id);
+    }
+
 }
