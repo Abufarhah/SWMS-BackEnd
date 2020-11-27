30e438a9b1f367fc090e26ad8022d6c4d2a2e035
Abufarhah <laythabufarhah@gmail.com>
2020-11-27 23:18:09 +0200

    fix password issue

--- src/main/java/edu/birzeit/swms/controllers/RegistrationController.java
+++ src/main/java/edu/birzeit/swms/controllers/RegistrationController.java
@@ -3,9 +3,7 @@ package edu.birzeit.swms.controllers;
 import edu.birzeit.swms.dtos.UserDto;
 import edu.birzeit.swms.models.ConfirmationToken;
 import edu.birzeit.swms.services.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -42,6 +40,7 @@ public class RegistrationController {
     }
 
     @GetMapping("/user")
+    @ApiOperation(value = "", authorizations = { @Authorization(value="jwtToken") })
     public UserDto getUser() {
         return userService.getUser();
     }
